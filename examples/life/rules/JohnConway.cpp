e11409be744ca064509a9318916311b9180f6e84
Henry F <henry.foley@mymail.champlain.edu>
2023-09-25 18:50:12 -0400

    Count Neighbors Update
    Still not working, but updating all the same

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,11 +3,11 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
 
-  for(int y = 0; y<world.SideSize(); y++)
+  for(int i = 0; i<world.SideSize(); i++)
   {
-    for(int x = 0; x<world.SideSize(); x++)
+    for(int j = 0; j<world.SideSize(); j++)
     {
-      Point2D point = Point2D(x,y);
+      Point2D point = Point2D(i,j);
       int neighbors = CountNeighbors(world, point);
       bool isAlive = world.Get(point);
       //world.SetNext(point, isAlive);
@@ -15,16 +15,14 @@ void JohnConway::Step(World& world) {
       //Get gets from the current buffer
       //Set sets to the next buffer
       //Rules 1 and 3
-
-
-      if (isAlive && neighbors< 2 || neighbors>3)
+      if (isAlive && neighbors<2 || neighbors>3)
       {
-        world.SetNext({x,y},false);
+        world.SetNext({i,j},false);
       }
       //Rule 4
       if(!isAlive && neighbors == 3)
       {
-        world.SetNext({x,y},true);
+        world.SetNext({i,j},true);
       }
     }
   }
@@ -32,21 +30,21 @@ void JohnConway::Step(World& world) {
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   int count = 0;
-  int boardSize = world.SideSize();
+  const int boardSize = world.SideSize();
 
-  for (int a = -1; a <= 1; a++)
+  for (int i = -1; i <= 1; i++)
   {
-    for(int b = -1; b <=1; b++)
+    for(int j = -1; j <=1; j++)
     {
-      bool isAlive = world.Get(Point2D(a, b));
-      if((a + point.x >= 0 && a + point.x < boardSize && b + point.y >= 0 && b + point.y < boardSize )
-          && (a!=0 || b!=0) && isAlive)
+      int x = i + point.x;
+      int y = j + point.y;
+      bool isAlive = world.Get(Point2D(x,y));
+      if((x >= 0 && x < boardSize && y >= 0 && y < boardSize)
+          && (i!=0 || j!=0) && isAlive)
       {
         count++;
       }
     }
   }
   return count;
-
-
 }
