30855c6b40b6c00eeddb983c152f96148012348f
Henry F <henry.foley@mymail.champlain.edu>
2023-09-26 13:36:04 -0400

    Working Game of Life

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -2,7 +2,6 @@
 
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-
   for(int i = 0; i<world.SideSize(); i++)
   {
     for(int j = 0; j<world.SideSize(); j++)
@@ -10,41 +9,36 @@ void JohnConway::Step(World& world) {
       Point2D point = Point2D(i,j);
       int neighbors = CountNeighbors(world, point);
       bool isAlive = world.Get(point);
-      //world.SetNext(point, isAlive);
+      world.SetNext(point, isAlive);
       //rules go here
       //Get gets from the current buffer
       //Set sets to the next buffer
       //Rules 1 and 3
-      if (isAlive && neighbors<2 || neighbors>3)
-      {
-        world.SetNext({i,j},false);
+      if(isAlive){
+        if(neighbors<2 || neighbors>3){
+          world.SetNext({i,j},false);
+        }
+        if(neighbors==2 || neighbors==3) {
+          world.SetNext({i,j},true);
+        }
       }
-      //Rule 4
-      if(!isAlive && neighbors == 3)
+      else
       {
-        world.SetNext({i,j},true);
+        if(neighbors == 3){
+          world.SetNext({i,j},true);
+        }
       }
     }
   }
 }
 
-int JohnConway::CountNeighbors(World& world, Point2D point) {
-  int count = 0;
-  const int boardSize = world.SideSize();
-
-  for (int i = -1; i <= 1; i++)
-  {
-    for(int j = -1; j <=1; j++)
-    {
-      int x = i + point.x;
-      int y = j + point.y;
-      bool isAlive = world.Get(Point2D(x,y));
-      if((x >= 0 && x < boardSize && y >= 0 && y < boardSize)
-          && (i!=0 || j!=0) && isAlive)
-      {
-        count++;
-      }
-    }
-  }
-  return count;
+int JohnConway::CountNeighbors(World& w, Point2D p) {
+    return w.Get(p.Up()) +
+         w.Get(p.Right().Up()) +
+         w.Get(p.Right()) +
+         w.Get(p.Right().Down()) +
+         w.Get(p.Down()) +
+         w.Get(p.Left().Down()) +
+         w.Get(p.Left()) +
+         w.Get(p.Left().Up());
 }
