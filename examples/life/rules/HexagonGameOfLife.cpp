2830f1f18a18ef04b56a43e1a53d07e157a74d84
Henry F <henry.foley@mymail.champlain.edu>
2023-09-26 20:13:24 -0400

    Started work on Hexagon

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -4,8 +4,32 @@
 
 #include "HexagonGameOfLife.h"
 void HexagonGameOfLife::Step(World& world) {
-
+  for(int i = 0; i<world.SideSize(); i++)
+  {
+    for(int j = 0; j<world.SideSize(); j++)
+    {
+      Point2D point = Point2D(i,j);
+      int neighbors = CountNeighbors(world, point);
+      bool isAlive = world.Get(point);
+      world.SetNext(point, isAlive);
+      if(isAlive){
+        if(neighbors<2 || neighbors>3){
+          world.SetNext({i,j},false);
+        }
+        if(neighbors==2 || neighbors==3) {
+          world.SetNext({i,j},true);
+        }
+      }
+      else
+      {
+        if(neighbors == 3){
+          world.SetNext({i,j},true);
+        }
+      }
+    }
+  }
 }
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
-  return 0;
+int HexagonGameOfLife::CountNeighbors(World& w, Point2D p) {
+  return w.Get(p.Up()) +
+         w.Get(p.Down());
 }
