be729c54e46369dbffbe0f6c09e334a65f91f626
Henry F <henry.foley@mymail.champlain.edu>
2023-10-10 20:06:43 -0400

    Still Gobilitygook
    BFS still wip

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <queue>
 #include <map>
+#include <unordered_set>
 
 Point2D Cat::Move(World* world) {
   auto rand = Random::Range(0, 5);
@@ -53,7 +54,6 @@ Point2D Cat::Seek(World* world)
       }
     }
 
-
     for(int i = 0; i < visitablePoints.size(); i++){
       // todo:Check if visitable point is in the closed list, if so ignore it
       /*for(int j = 0; j < openList.size(); j++){
@@ -84,24 +84,50 @@ Point2D Cat::Seek(World* world)
 Point2D Cat::GenerateTarget(World* world)
 {
   auto catPos = world->getCat();
-  std::priority_queue<Point2D> queue;
-  std::map<Point2D,Point2D> cameFrom;
-  cameFrom[catPos] = Point2D(0,0);
+  std::queue<Point2D> queue;
+  std::unordered_set<Point2D> visited;
+  std::map<int, std::map<int,Point2D>> cameFrom;
+  cameFrom[catPos.y][catPos.x] = Point2D(0,0);
 
-  //Add Cat's initial position to the open list
+  //Add Cat's initial position to the open list and marked visited
   queue.push(catPos);
+  visited.insert(catPos);
 
   //BFS Algorithm
   while(!queue.empty()){
-    auto current = queue.top();
-    //todo:If point is outside of the grid space
-    if(current == OUTSIDE_OF_GRIDSPACE){
+    auto current = queue.front();
+    queue.pop();
+
+    //If cat wins on space then break the loop
+    if(world->catWinsOnSpace(current)){
       break;
     }
     for(int i=0; i <World::neighbors(current).size(); i++){
       if(!cameFrom[i]){
         queue.push(cameFrom[i]);
         cameFrom[i] = current;
+      }
+    }
+
+    // todo:Calculate
+    auto neighbors = world->neighbors(current);
+    for(int i=0; i <visited.size(); i++)
+    {
+      if(neighbors[i] == visited[i])
+      {
+        break;
+      }
+    }
+
+    for(int i=0; i <neighbors.size(); i++){
+      // todo:If current neighbor is not blocked then
+
+      if(world->isValidPosition(neighbors[i]) &&
+          world->getContent(neighbors[i])     &&
+          neighbors[i] != world->getCat())
+      {
+
+
       }
     }
   }
